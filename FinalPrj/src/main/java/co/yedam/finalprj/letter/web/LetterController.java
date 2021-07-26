65edb9aee87700351cad71f138a7b5c7b57c72e3
eumshee <esh_317@naver.com>
2021-07-26 17:54:32 +0900

    편지 조회, 편지함 레이아웃 수정중

--- FinalPrj/src/main/java/co/yedam/finalprj/letter/web/LetterController.java
+++ FinalPrj/src/main/java/co/yedam/finalprj/letter/web/LetterController.java
@@ -1,5 +1,35 @@
 package co.yedam.finalprj.letter.web;
 
-public class LetterController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import co.yedam.finalprj.letter.service.LetterService;
+import co.yedam.finalprj.letter.vo.LetterVO;
+
+@Controller
+public class LetterController {
+	@Autowired
+	LetterService letterDao;
+	
+	@RequestMapping("letterBox.do")
+	public String letterBox(Model model, LetterVO vo) {
+		vo.setTo_id("user3");
+		model.addAttribute("newLetter", letterDao.selectNewLetter(vo));
+		return "letter/letterBox";
+	}
+
+	@RequestMapping("arriveLetter.do")
+	public String ArriveLetter(Model model, LetterVO vo) {
+		vo.setTo_id("user3");		
+		model.addAttribute("friends", letterDao.selectAllFriend(vo));		
+		model.addAttribute("friendLetter", letterDao.selectFriendLetter(vo));		
+		return "letter/arriveLetter";
+	}
+	
+	@RequestMapping("savedLetter.do")
+	public String SavedLetter() {
+		return "letter/savedLetter";
+	}
 }
