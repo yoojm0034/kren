871c7f4a0efaada33044bd6642bf89adc4cdb226
admin <admin@YD02-09>
2021-07-29 17:52:07 +0900

    i commit

---
+++ FinalPrj/src/main/java/co/yedam/finalprj/users/handler/AccessDeniedHandler.java
@@ -0,0 +1,28 @@
+package co.yedam.finalprj.users.handler;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.stereotype.Service;
+
+@Service("accessFailHandler")
+public class AccessDeniedHandler implements org.springframework.security.web.access.AccessDeniedHandler{
+
+	@Override
+	public void handle(HttpServletRequest request, HttpServletResponse response,
+			AccessDeniedException accessDeniedException) throws IOException, ServletException {
+
+		if(accessDeniedException instanceof AccessDeniedException) {
+			request.setAttribute("LoginFailMessage", "접근 권한이 없는 계정입니다.");
+		}
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/home.do");
+		dispatcher.forward(request, response);
+		
+	}
+
+}
