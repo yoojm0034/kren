33ebab8dadd72bcd7e66eea8190c34a9cdb426bc
admin <admin@YD02-09>
2021-08-03 17:52:56 +0900

    마이페이지-우표함

--- FinalPrj/src/main/java/co/yedam/finalprj/stamph/service/StamphService.java
+++ FinalPrj/src/main/java/co/yedam/finalprj/stamph/service/StamphService.java
@@ -1,5 +1,8 @@
 package co.yedam.finalprj.stamph.service;
 
-public interface StamphService {
+import co.yedam.finalprj.stamph.vo.StamphVO;
 
+public interface StamphService {
+	StamphVO stamphSelect(StamphVO vo);
+	int stamphInsert(StamphVO vo);
 }
