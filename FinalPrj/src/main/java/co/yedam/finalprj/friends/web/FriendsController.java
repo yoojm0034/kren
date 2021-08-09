58edff8c65f90f13fd4d7c0f216053fe8bd0317f
admin <admin@YD02-16>
2021-08-09 18:55:15 +0900

    search Friend 삭제가 안된다요

--- FinalPrj/src/main/java/co/yedam/finalprj/friends/web/FriendsController.java
+++ FinalPrj/src/main/java/co/yedam/finalprj/friends/web/FriendsController.java
@@ -13,14 +13,23 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import co.yedam.finalprj.feed.service.FeedService;
 import co.yedam.finalprj.friends.service.FriendsService;
 import co.yedam.finalprj.friends.vo.FriendsVO;
+import co.yedam.finalprj.topic.service.TopicService;
+import co.yedam.finalprj.users.service.UsersService;
 import co.yedam.finalprj.users.vo.UsersVO;
 
 @Controller
 public class FriendsController {
 	
 	@Autowired FriendsService FriendsDao;
+	
+	@Autowired FeedService feedDao;
+	
+	@Autowired UsersService userDao;
+	
+	@Autowired TopicService topicDao;
 	
 	@RequestMapping("friendSearch.do")
 	public String feiendSearch() {
@@ -59,5 +68,77 @@ public class FriendsController {
 			return "UnFollowOK";
 		}
 		return "fail";		
-	} 
+	}
+	
+
+	//친구검색화면 
+	@RequestMapping("friendSearch11.do")
+	public String allUserList1(UsersVO vo,Model model,Authentication auth){
+		User user = (User) auth.getPrincipal();
+		String id = (String) user.getUsername();
+		
+		vo.setUser_id(id);
+		vo = userDao.usersSelect(vo);	
+		vo.setTopic(vo.getTopic());
+		System.out.println(vo);
+		model.addAttribute("allList",feedDao.allUser(vo));
+		model.addAttribute("searchList",feedDao.allUser(vo));
+		model.addAttribute("newList",feedDao.newUser(vo));
+		model.addAttribute("topicList",topicDao.topicSelectList());
+		return "friends/friendSearch";
+	};
+	
+	//친구검색
+	@RequestMapping("searchList1.do")
+	public String friendSearchList1(Model model,UsersVO vo,Authentication auth) {
+		User user = (User) auth.getPrincipal();
+		String id = (String) user.getUsername();
+
+		vo.setUser_id(id);
+		
+		if(vo.getS_age().equals("") || vo.getE_age().equals("") ){
+			vo.setS_age(null);
+		}
+		if(vo.getS_dage().equals("") || vo.getE_dage().equals("") ){
+			vo.setS_dage(null);
+		}
+		if(vo.getGender().equals("")) {
+			vo.setGender(null);
+		}
+		if(vo.getDgender().equals("")) {
+			vo.setDgender(null);
+		}
+		if(vo.getCountry().equals("")) {
+			vo.setCountry(null);
+		}
+		if(vo.getDcountry().equals("")) {
+			vo.setDcountry(null);
+		}
+		if(vo.getLanguage1().equals("")) {
+			vo.setLanguage1(null);
+		}
+		if(vo.getTopic().equals("")) {
+			vo.setTopic(null);
+		}
+		if(vo.getDtopic().equals("")) {
+			vo.setDtopic(null);
+		}
+		
+		System.out.println("시작 나이: "+vo.getS_age());
+		System.out.println("시작 나이2: "+vo.getE_age());
+		System.out.println("끝 나이: "+vo.getS_dage());
+		System.out.println("끝 나이2: "+vo.getE_dage());
+		System.out.println("성별: "+vo.getGender());
+		System.out.println("국가: "+vo.getCountry());
+		System.out.println("제외국가: "+vo.getDcountry());
+		System.out.println("언어: "+vo.getLanguage1());
+		System.out.println("토픽: "+vo.getTopic());
+		System.out.println("제외토픽: "+vo.getDtopic());
+		
+		System.out.println(feedDao.searchFriend(vo));
+
+		model.addAttribute("searchList",feedDao.searchFriend(vo));
+		return "friendSearch1.do";
+	}
+	
 }
