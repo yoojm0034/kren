3fae468d1b728e8af9b536ddb19441a15a1a5fa6
admin <admin@YD02-03>
2021-08-10 11:30:58 +0900

    feed controller

--- FinalPrj/src/main/java/co/yedam/finalprj/feed/web/FeedController.java
+++ FinalPrj/src/main/java/co/yedam/finalprj/feed/web/FeedController.java
@@ -15,7 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.ui.Model;  
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
