d98b95fd6f4e815e42a074cd0464839b6ae34ec1
jennachoi <cjw782@gmail.com>
2021-08-10 14:13:48 +0900

    시큐리티수정

--- FinalPrj/src/main/java/co/yedam/finalprj/users/web/UsersController.java
+++ FinalPrj/src/main/java/co/yedam/finalprj/users/web/UsersController.java
@@ -223,52 +223,55 @@ public class UsersController {
 	@RequestMapping("find/mailCheck.do")
 	@ResponseBody
 	public int mailCheck(HttpServletRequest request, UsersVO vo) throws IOException {
-		UsersVO uvo = usersDao.emailCheck(vo.getEmail()); //쿼리 값을 받아옴
+		String email = vo.getEmail();
+		int uvo = usersDao.emailCheck(email); //쿼리 값을 받아옴
 		
-		if(uvo == null) {
+		if(uvo == 0) {
 			//DB에 동일 이메일이 없을 경우...0으로 뷰의 res에 넘긴다.
 			return 0;
+		} else {
+			
+			Random r = new Random();
+	        int dice = r.nextInt(4589362) + 49311; //이메일로 받는 인증코드 부분 (난수)
+	        
+	        String setfrom = "5524yina@gamil.com"; //보내는 사람 이메일(삭제하면 기능하지않음)
+	        String tomail = vo.getEmail(); // 받는 사람 이메일
+	        String title = "[kren]아이디 찾기-이메일 인증번호 발송"; // 이메일 제목
+	        String content =
+	        
+	        System.getProperty("line.separator")+ //줄간격을 띄우기 위한 코드
+	        
+	        System.getProperty("line.separator")+
+
+	        " 인증번호 " +dice+ ". "
+	        
+	        +System.getProperty("line.separator")+
+	        
+	        System.getProperty("line.separator")+
+	        
+	        "인증번호를 홈페이지에 입력해 주세요."; // 내용
+	        
+	        request.getSession().setAttribute("dice", dice); //인증번호 세션에 담아둠
+	        
+	        try {
+	            MimeMessage message = mailSender.createMimeMessage();
+	            MimeMessageHelper messageHelper = new MimeMessageHelper(message, true, "UTF-8");
+
+	            messageHelper.setFrom(setfrom); // 보내는사람 생략하면 정상작동을 안함
+	            messageHelper.setTo(tomail); // 받는사람 이메일
+	            messageHelper.setSubject(title); // 메일제목은 생략이 가능하다
+	            messageHelper.setText(content); // 메일 내용
+	            
+	            mailSender.send(message);
+	        } catch (Exception e) {
+	            System.out.println(e);
+	        }  
+	        return 1;
 		}
 		
 		//DB에 동일 이메일이 있으면...1로 뷰의 res에 넘기며 이메일을 발송한다.
-		
-		Random r = new Random();
-        int dice = r.nextInt(4589362) + 49311; //이메일로 받는 인증코드 부분 (난수)
+	
         
-        String setfrom = "5524yina@gamil.com"; //보내는 사람 이메일(삭제하면 기능하지않음)
-        String tomail = vo.getEmail(); // 받는 사람 이메일
-        String title = "[kren]아이디 찾기-이메일 인증번호 발송"; // 이메일 제목
-        String content =
-        
-        System.getProperty("line.separator")+ //줄간격을 띄우기 위한 코드
-        
-        System.getProperty("line.separator")+
-
-        " 인증번호 " +dice+ ". "
-        
-        +System.getProperty("line.separator")+
-        
-        System.getProperty("line.separator")+
-        
-        "인증번호를 홈페이지에 입력해 주세요."; // 내용
-        
-        request.getSession().setAttribute("dice", dice); //인증번호 세션에 담아둠
-        
-        try {
-            MimeMessage message = mailSender.createMimeMessage();
-            MimeMessageHelper messageHelper = new MimeMessageHelper(message, true, "UTF-8");
-
-            messageHelper.setFrom(setfrom); // 보내는사람 생략하면 정상작동을 안함
-            messageHelper.setTo(tomail); // 받는사람 이메일
-            messageHelper.setSubject(title); // 메일제목은 생략이 가능하다
-            messageHelper.setText(content); // 메일 내용
-            
-            mailSender.send(message);
-        } catch (Exception e) {
-            System.out.println(e);
-        }  
-        
-        return 1;
 	}
 	
 	/*
