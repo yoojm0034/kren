f2dd7cc420a54152794aedf9187bec3075f701d4
admin <admin@YD02-03>
2021-08-11 17:52:16 +0900

    통계페이지 구현중

--- FinalPrj/src/main/java/co/yedam/finalprj/common/map/VisitCountMap.java
+++ FinalPrj/src/main/java/co/yedam/finalprj/common/map/VisitCountMap.java
@@ -1,8 +1,11 @@
 package co.yedam.finalprj.common.map;
 
+import java.util.List;
+import java.util.Map;
 
 public interface VisitCountMap {
 	public int getVisitTotalCount();
 	public int getVisitToday();
 	public int insertVisitor();
+	public List<Map<String, Object>> dayCount();
 }
--- FinalPrj/src/main/java/co/yedam/finalprj/common/service/VisitCountService.java
+++ FinalPrj/src/main/java/co/yedam/finalprj/common/service/VisitCountService.java
@@ -1,8 +1,11 @@
 package co.yedam.finalprj.common.service;
 
+import java.util.List;
+import java.util.Map;
 
 public interface VisitCountService {
 	public int insertVisitor();
 	public int getVisitTotalCount();
 	public int getVisitToday();
+	public List<Map<String, Object>> dayCount();
 }
